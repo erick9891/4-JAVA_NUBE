79d5da01f2eb93e8f435971862dce7e52da327ec
Erick Ore <erick989@gmail.com>
2016-05-29 13:44:35 -0500

    Actualizando repositorio

---
+++ Demo01/Demo01/src/java/pe/eeob/controller/MateController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.eeob.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import pe.eeob.service.MateService;
+
+/**
+ *
+ * @author Alumno
+ */
+@ManagedBean
+@RequestScoped
+public class MateController {
+
+    private int num1;
+    private int num2;
+    private int suma;
+    private boolean formulario=true;
+    
+    public boolean isFormulario(){
+        return formulario;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getSuma() {
+        return suma;
+    }
+
+    public void setSuma(int suma) {
+        this.suma = suma;
+    }
+    
+    public void doProcesar(){
+        MateService service = new MateService();
+        suma = service.sumar(num1, num2);
+        formulario = false;
+    }
+    
+    public void doRetornar(){
+        formulario = true;
+    }
+}
